9db7bb73ae0974f15816cc4ae37f31ba7bbe70c6
asadullah-dal-17 <asadullah.dal17@gmail.com>
2021-08-05 06:31:10 +0500

    color and blur background of text fucntion added

---
+++ utils.py
@@ -0,0 +1,72 @@
+import cv2 as cv 
+import numpy as np
+
+def textWithBackground(img, text, font, fontScale, textPos, textThickness=1,textColor=(0,255,0), bgColor=(0,0,0), pad_x=3, pad_y=3, bgOpacity=0.5):
+    """
+    Draws text with background, with  control transparency
+    @param img:(mat) which you want to draw text
+    @param text: (string) text you want draw
+    @param font: fonts face, like FONT_HERSHEY_COMPLEX, FONT_HERSHEY_PLAIN etc.
+    @param fontScale: (double) the size of text, how big it should be.
+    @param textPos: tuple(x,y) position where you want to draw text
+    @param textThickness:(int) fonts weight, how bold it should be
+    @param textPos: tuple(x,y) position where you want to draw text
+    @param textThickness:(int) fonts weight, how bold it should be.
+    @param textColor: tuple(BGR), values -->0 to 255 each
+    @param bgColor: tuple(BGR), values -->0 to 255 each
+    @param pad_x: int(pixels)  padding of in x direction
+    @param pad_y: int(pixels) 1 to 1.0 (), controls transparency of  text background 
+    @return: img(mat) with draw with background
+    """
+    (t_w, t_h), _= cv.getTextSize(text, font, fontScale, textThickness) # getting the text size
+    x, y = textPos
+    overlay = img.copy() # coping the image
+    cv.rectangle(overlay, (x-pad_x, y+ pad_y), (x+t_w+pad_x, y-t_h-pad_y), bgColor,-1) # draw rectangle 
+    new_img = cv.addWeighted(overlay, bgOpacity, img, 1 - bgOpacity, 0) # overlaying the rectangle on the image.
+    cv.putText(new_img,text, textPos,font, fontScale, textColor,textThickness ) # draw in text
+    img = new_img
+
+    return img
+
+
+def textBlurBackground(img, text, font, fontScale, textPos, textThickness=1,textColor=(0,255,0),kneral=(33,33) , pad_x=3, pad_y=3):
+    """
+    
+    Draw text with background blured,  control the blur value, with kernal(odd, odd)
+    @param img:(mat) which you want to draw text
+    @param text: (string) text you want draw
+    @param font: fonts face, like FONT_HERSHEY_COMPLEX, FONT_HERSHEY_PLAIN etc.
+    @param fontScale: (double) the size of text, how big it should be.
+    @param textPos: tuple(x,y) position where you want to draw text
+    @param textThickness:(int) fonts weight, how bold it should be.
+    @param textColor: tuple(BGR), values -->0 to 255 each
+    @param kneral: tuple(3,3) int as odd number:  higher the value, more blurry background would be
+    @param pad_x: int(pixels)  padding of in x direction
+    @param pad_y: int(pixels)  padding of in y direction
+    @return: img mat, with text drawn, with background blured
+    
+    call the function: 
+     img =textBlurBackground(img, 'Blured Background Text', cv2.FONT_HERSHEY_COMPLEX, 0.9, (20, 60),2, (0,255, 0), (49,49), 13, 13 )
+    """
+    
+    (t_w, t_h), _= cv.getTextSize(text, font, fontScale, textThickness) # getting the text size
+    x, y = textPos
+    blur_roi = img[y-pad_y-t_h: y+pad_y, x-pad_x:x+t_w+pad_x] # croping Text Background
+    img[y-pad_y-t_h: y+pad_y, x-pad_x:x+t_w+pad_x]=cv.blur(blur_roi, kneral)  # merging the blured background to img
+    cv.putText(img,text, textPos,font, fontScale, textColor,textThickness )          
+    # cv.imshow('blur roi', blur_roi)
+    # cv.imshow('blured', img)
+    return img
+
+def main():
+    cap = cv.VideoCapture(0)
+    while True:
+        success, img = cap.read()
+        textBlurBackground(img, 'Blured Background Text', cv.FONT_HERSHEY_COMPLEX, 1.4, (60, 140),3, (0,255, 0), (71,71), 13, 13)
+        img=textWithBackground(img, 'Colored Background Texts', cv.FONT_HERSHEY_COMPLEX, 1.6, (60,70), textThickness=3, bgColor=(0,255,0), textColor=(0,0, 0), bgOpacity=0.5, pad_x=10, pad_y=10)
+        imgGray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+        cv.imshow('img', img)
+        if cv.waitKey(1) ==ord('q'):
+            break
+if __name__ == "__main__":
+    main()
